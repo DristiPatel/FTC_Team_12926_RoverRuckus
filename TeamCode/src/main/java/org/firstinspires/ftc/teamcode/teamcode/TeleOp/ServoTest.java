b510ef4052f7943f584ba77d9ca0ebee9b2d4f54
Dristi Patel <dristipatel@gmail.com>
2018-11-06 17:24:53 -0600

    check update log

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/TeleOp/ServoTest.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class ServoTest extends OpMode {
+
+
+    private Servo servo1, servo2;
+
+    public void init(){
+
+        servo1 = hardwareMap.get(Servo.class, "Servo1");
+        servo2 = hardwareMap.get(Servo.class, "Servo2");
+    }
+
+
+    public void loop(){
+
+        if(gamepad1.dpad_up){
+
+            servo1.setPosition(0);
+            servo2.setPosition(.5);
+
+        }else if(gamepad1.dpad_down){
+
+            servo1.setPosition(.5);
+            servo2.setPosition(1);
+
+        }
+
+
+    }
+
+
+}
